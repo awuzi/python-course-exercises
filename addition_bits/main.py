75d9599481f297ce908b07b1b46750d6b7abb510
Yahia <yahia@chefing.fr>
2021-11-03 20:54:43 +0100

    use zip function

--- addition_bits/main.py
+++ addition_bits/main.py
@@ -1,37 +1,27 @@
-_xor = lambda a, b: a ^ b
-# def _xor(a: int, b: int) -> int:
-#     return a ^ b
+def _xor(a: int, b: int) -> int:
+    return a ^ b
 
-_and = lambda a, b: a & b
-# def _and(a: int, b: int) -> int:
-#     return a & b
 
-_or = lambda a, b: a | b
-# def _or(a: int, b: int) -> int:
-#     return a | b
+def _and(a: int, b: int) -> int:
+    return a & b
+
+
+def _or(a: int, b: int) -> int:
+    return a | b
 
 
 def add_two_bits(a: int, b: int, carry: int):
-    """
-    :param a: bit
-    :param b: bit
-    :param carry: cost when 1 + 1
-    :return: operation and carry
-    """
     res: int = _xor(_xor(a, b), carry)
     carry: int = _or(_and(_xor(a, b), carry), _and(a, b))
 
     return res, carry
 
 
-def add_two_binay_num(a: str, b: str) -> [int]:
+def add_two_binay_num(a: str, b: str) -> "list[int]":
     carry = 0
     result = []
-    reversed_idx = len(a) - 1
-    for (idx, elem) in enumerate(a):
-        first_bit = int(a[reversed_idx - idx])
-        second_bit = int(b[reversed_idx - idx])
-        operation_result, cost = add_two_bits(first_bit, second_bit, carry)
+    for (elem_a, elem_b) in zip(a, b):
+        [operation_result, cost] = add_two_bits(int(elem_a), int(elem_b), carry)
         carry = cost
         result.insert(0, operation_result)
 
